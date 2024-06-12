91156dc871907d144d74873ef1b7fa2bf195ac1f
redi-yasin-umer <rediyasin4@gmail.com>
2024-06-12 14:24:53 +0300

    22. Add Drawer Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,16 @@ import Header from '@/components/header/header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Providers from '../components/Providers'
+import Providers from '@/components/Providers'
+import DrawerButton from '../components/DrawerButton'
+import Sidebar from '../components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'red-1',
+  title: 'Next Amazona V2',
   description: 'Modern ECommerce Website',
 }
-
 export default function RootLayout({
   children,
 }: {
@@ -20,14 +21,29 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-            <div className="min-h-screen flex flex-col">
-              <Header />
-              {children}
-              <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-                <p>Copyright © 2023 - All right reserved by Next Amazona V2</p>
-              </footer>
+          <div className="drawer">
+            <DrawerButton />
+            <div className="drawer-content">
+              <div className="min-h-screen flex flex-col">
+                <Header />
+                {children}
+                <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                  <p>
+                    Copyright © 2023 - All right reserved by Next Amazona V2
+                  </p>
+                </footer>
+              </div>
             </div>
-          </Providers>
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <Sidebar />
+            </div>
+          </div>
+        </Providers>
       </body>
     </html>
   )
