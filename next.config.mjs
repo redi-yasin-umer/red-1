6940a1b6dd08f4a1a9006403262001e3bb0b28ff
redi-yasin-umer <rediyasin4@gmail.com>
2024-06-12 20:37:47 +0300

    28. Upload Product Images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com', // if your website has no www, drop it
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
+}
 
 export default nextConfig;
