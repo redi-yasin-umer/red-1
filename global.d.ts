8dd4483e4282ea63358f1351e52b8ed57a323c89
redi-yasin-umer <rediyasin4@gmail.com>
2024-06-12 08:32:03 +0300

    20. Publish Website On Vercel

---
+++ global.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  namespace NodeJS {
+    interface Global {
+      mongoose: {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+      };
+    }
+  }
+}
+
+// This is necessary to make the file a module
+export {};
